08ed79dbb9a830ec83d29283d254c0e99f8d7f90
Marzbani <hamied.reza.m@gmail.com>
2022-11-14 02:23:30 +0330

    [FEA] homepage_screen.dart widgets refactored based BaseView class

--- lib/feature/home/presentation/pages/homepage_screen.dart
+++ lib/feature/home/presentation/pages/homepage_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_clean_architecture_bloc/app/base/base_view.dart';
 import 'package:flutter_clean_architecture_bloc/app/messages/app_messages.dart';
+import 'package:flutter_clean_architecture_bloc/feature/auth/data/models/user_model.dart';
 import 'package:flutter_clean_architecture_bloc/feature/home/presentation/bloc/home_bloc.dart';
 import 'package:flutter_clean_architecture_bloc/feature/home/presentation/bloc/home_event.dart';
 import 'package:flutter_clean_architecture_bloc/feature/home/presentation/bloc/home_state.dart';
@@ -18,7 +19,21 @@ class HomePage extends BaseView {
         );
     return super.build(context);
   }
+  @override
+  PreferredSizeWidget? appBar(BuildContext context) {
+    return AppBar(
+      title: const Text(APPMessages.homepage),
+    );
+  }
+  @override
+  Widget? drawer() {
 
+    return BlocConsumer<HomeBloc, HomeState>(builder: (context, state) {
+      return state is HomepageLoadedState ?  HomeDrawer(user: state.user) : const HomeDrawer(user: UserModel());
+    }, listener: (context, state) {
+
+    },);
+  }
   @override
   Widget body(BuildContext context) {
     return BlocConsumer<HomeBloc, HomeState>(listener: (context, state) {
@@ -28,37 +43,30 @@ class HomePage extends BaseView {
       //       );
       // }
     }, builder: (context, state) {
-      return Scaffold(
-        drawer:
-            state is HomepageLoadedState ? HomeDrawer(user: state.user) : null,
-        appBar: AppBar(
-          title: const Text(APPMessages.homepage),
-        ),
-        body: state is HomeLoadingState
-            ? const Center(
-                child: CircularProgressIndicator(),
-              )
-            : state is HomepageLoadedState
-                ? ListView.separated(
-                    separatorBuilder: (_, __) => const Divider(),
-                    itemCount: state.productList.products!.length,
-                    itemBuilder: (context, index) {
-                      final item = state.productList.products![index];
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(item.thumbnail),
-                        ),
-                        title: Text(item.title),
-                        subtitle: Text(
-                          item.description,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      );
-                    },
-                  )
-                : Container(),
-      );
+      return state is HomeLoadingState
+          ? const Center(
+        child: CircularProgressIndicator(),
+      )
+          : state is HomepageLoadedState
+          ? ListView.separated(
+        separatorBuilder: (_, __) => const Divider(),
+        itemCount: state.productList.products!.length,
+        itemBuilder: (context, index) {
+          final item = state.productList.products![index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(item.thumbnail),
+            ),
+            title: Text(item.title),
+            subtitle: Text(
+              item.description,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          );
+        },
+      )
+          : Container();
     });
   }
 }
